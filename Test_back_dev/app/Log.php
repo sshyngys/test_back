52a11d5e43c5a3d7a58e0eeeafa1a4aa901bbc62
sshyngys <78813731+sshyngys@users.noreply.github.com>
2021-08-23 22:34:25 +0600

    test-back

---
+++ Test_back_dev/app/Log.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+class Log
+{
+	public static function write($action)
+	{
+		DB::table('history')->insert(['action' => $action, 'create_at' => time()]);
+	}
+}
